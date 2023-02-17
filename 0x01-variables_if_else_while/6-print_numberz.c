9809e57d1bcff1832e75103c0cf56083e7f79ab9
oluwafolahanmi <adebowale304@gmail.com>
2023-02-17 13:27:39 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: to 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
