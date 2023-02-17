9b438e3572f2a44b30a1c23381e440f48f49c559
oluwafolahanmi <adebowale304@gmail.com>
2023-02-17 14:16:35 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,13 @@ int main(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
