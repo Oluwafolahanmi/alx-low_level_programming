e84d229c8ac0908d6b72d726c63f478783bc4e07
oluwafolahanmi <adebowale304@gmail.com>
2023-02-22 03:37:17 -0800

    task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0, b = 1, c = 0;
+	int sum = 0;
+
+	while (c < 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+			sum += c;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
