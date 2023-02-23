22f873ddeb8c635ffb0ebb114263d3db323f46d3
oluwafolahanmi <adebowale304@gmail.com>
2023-02-23 09:39:13 -0800

    j

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - main function
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48 ; j < 58 ; j++)
+		_putchar(j);
+	_putchar('\n');
+}
