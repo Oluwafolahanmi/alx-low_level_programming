7d68e10c9087e7673e8166e54385518f89f5c8c1
oluwafolahanmi <adebowale304@gmail.com>
2023-02-23 08:33:02 -0800

    iu

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - main function
+ * @c: function parameter
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	for (c = 0 ; c < 10 ; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
+
